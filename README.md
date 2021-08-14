37219237bb4b5fe6d9d95372b21c55a44da05fee
RogueEffect <therogueeffect@gmail.com>
2021-08-13 22:07:19 -0700

    Part 3 - User Input
    Shuffling the board and getting user input

--- README.md
+++ README.md
@@ -1,41 +1,15 @@
 
-Problem 2
-    [commit](https://github.com/RogueEffect/slide_puzzle_c/commit/55a1ea7a6132a1289493e938151bd86ca18cbdf3)
+Problem 3
 
-How can we handle moves?
+Shuffling tiles and getting user input
 
-On a physical slide puzzle we can slide a tile into the space. In a digital
-  version we can accomplish the same by swapping the space with an adjacent tile
+We can shuffle the board by creating a loop that performs a few hundred valid
+moves on the board. Performing actual moves is important as just shuffling
+the tiles could create an unsolvable puzzle
 
-Consider the move below where we start with the solved board and swap L and space
-
-```
-  ABCD    ABCD
-  EFGH -> EFGH
-  IJKL    IJK 
-  MNO     MNOL
-```
-
-Actually swapping tiles should be trivial, however we have to consider if a move
-  is valid or not beforehand. Another thing to consider is how can we find out
-  which tiles are able to move?
-
-A simple way to handle a move would be to keep track of the spacer tile and do
-  moves based around its location. Getting surrounding tiles will require more
-  math, but it should be fairly simple
-
-If you have the x and y location of the spacer tile you can find the following
-  adjacent tiles with some simple arithmatic
-
-The tile above the space will be at (x, y - 1)\
-The tile below will be at (x, y + 1)\
-Left is (x - 1, y)\
-and right is (x + 1, y)
-
-These four moves should be the only valid moves in the game (assuming we dont try
-  moving the space out of the board). An easy way to avoid going out of bounds
-  would be to check that the position of the adjacent tile is 0 <= x < width
-  and 0 <= y < height
-
-Now we can work on two functions, one to handle moves and one to actually swap
-  the tiles
+As for input, a fancier console program would use something like conio or curses
+to draw to the screen and get input. For this project I'm going to keep it
+simple and print the board for every move the user makes. We can use simple
+getc calls and do some action based on the character given. We will also want
+to have a loop to keep asking for input until the puzzle is solved or the
+player quits
--- src/puzzle.c
+++ src/puzzle.c
@@ -10,44 +10,43 @@ rogueeffect
 */
 
 #include <stdio.h>
-#include <string.h> 
+#include <stdlib.h> // For rand and srand
+#include <string.h>
+#include <time.h> // For time function
 
 #define WIDTH   4
 #define HEIGHT  4
 #define SQUARE_SIZE     2
 #define BOARD_WIDTH     (SQUARE_SIZE * WIDTH + 1)
 #define BOARD_HEIGHT    (SQUARE_SIZE * HEIGHT + 1)
-#define INDENT  4 
+// I originally wanted to make the indent change depending on the board size
+#define INDENT  "    " // but I'm changing this to a static string instead
+// Macro to generate a random number from 0 to max - 1
+#define randint(max)    (rand() % max)
 
 char    tiles[WIDTH * HEIGHT];
-// This can be the index of the space tile
+
 int     space;
+int     moves;
 
 void    init();
 void    draw_board();
 void    swap(int, int);
 int     handle_move(int, int);
+void    shuffle_board();
+int     game_loop();
+void    clear_input();
 
 
 
 int main(int argc, char** argv) {
-    printf("Testing moving tiles...\n");
+    // Seed random number generator
+    srand(time(0));
+    
+    printf("Testing user input...\n");
     init();
-    draw_board();
-    // Here we can test our new functions (check the definitions at the end)
-    handle_move(0, -1); // move space up
-    draw_board();
-    handle_move(-1, 0); // move space left twice
-    handle_move(-1, 0);
-    draw_board();
-    handle_move(0, 1); // move space down
-    draw_board();
-    handle_move(1, 0); // move space right
-    draw_board();
-    // Let's test if invalid moves do anything
-    if(!handle_move(0, 1)) {
-        printf("Invalid move!\n");
-    }
+    // Run the game loop until it returns false
+    while(game_loop());
     
     printf("done.\n");
 }
@@ -60,9 +59,11 @@ void init() {
         tiles[i] = c;
     }
     
-    // Set the location of the space tile (move handling will rely on this)
     space = WIDTH * HEIGHT - 1;
     tiles[space] = ' ';
+    shuffle_board();
+    // Make sure to do this after we shuffle since that will increment the
+    moves = 0; // move counter when we shuffle
 }
 
 void draw_board() {
@@ -81,44 +82,118 @@ void draw_board() {
             buffer[x] = c;
         }
         buffer[BOARD_WIDTH] = '\0';
-        char indent[INDENT + 1];
-        memset(indent, ' ', INDENT);
-        indent[INDENT] = '\0';
-        printf("%s%s\n", indent, buffer);
+        // changed the indent here
+        printf("%s%s\n", INDENT, buffer);
     }
     printf("\n");
 }
 
-// This function swaps 2 tiles given their indices
 void swap(int a, int b) {
     char temp = tiles[a];
     tiles[a] = tiles[b];
     tiles[b] = temp;
 }
 
-// Here we take in 2 values representing the delta in x and y directions
-// Each of the four moves provides one non-zero value and one zero value but
-//   never two non-zeros, or two zeros (that wouldnt even move anything)
 int handle_move(int dx, int dy) {
-    // Let's assume we will always used this function properly and leave out any
-    //   sanity checks about diagonal or useless moves
-    int x = space % WIDTH; // Again some math because im using 1d array
+    int x = space % WIDTH;
     int y = space / WIDTH;
     dx += x;
     dy += y;
-    // Let's check that the target x location is valid
-    if(dx < 0 || dx >= WIDTH) return 0; // The move failed, warn the user
-    // Now the y location
+   
+    if(dx < 0 || dx >= WIDTH) return 0;
     if(dy < 0 || dy >= HEIGHT) return 0;
-    // If we get here the move is valid and we can perform the swap
+   
     swap(space, dx + dy * WIDTH);
-    // Then update where the space is
     space = dx + dy * WIDTH;
-    // Later we can implement move counting here
-    return 1; // Move succeeded
+    moves++; // Only increment move counter on a valid move
+    return 1;
 }
 
-// Remember handle move will always take one zero value and one non-zero (1, -1)
-//   otherwise the space will make movements that could break the puzzle
-// Next we'll have to map the four moves to something the user can type into
-//   the console. (Probably WASD since arrow keys cannot be sent to stdin)
+// Shuffle the board by doing random moves
+void shuffle_board() {
+    // We'll base the amount of moves on the size of the board
+    int target = WIDTH * HEIGHT * 20;
+    int dx, dy;
+    while(target > 0) {
+        // Pick a random number from 0 - 3. We'll set up dx and dy to handle
+        //   one of the four directions
+        switch(randint(4)) {
+            case 0: // Up
+                dx = 0;
+                dy = -1;
+                break;
+            case 1: // Down
+                dx = 0;
+                dy = 1;
+                break;
+            case 2: // Left
+                dx = -1;
+                dy = 0;
+                break;
+            case 3: // Right
+                dx = 1;
+                dy = 0;
+                break;
+        }
+        // Only count down if the move is valid
+        if(handle_move(dx, dy)) target--;
+    }
+    // Next time we should make sure the shuffle didnt solve itself here
+}
+
+// Draw the board and ask for user input
+int game_loop() {
+    draw_board();
+    // We should check here for the win condition
+    // Print an indent then the amount of moves then a prompt for input
+    //   the %-4d specifier will print a number left justified with a width
+    //   of 4. This means if the number is less than 4 digits wide it will
+    //   add spaces to make it 4 wide
+    printf("%s%-4d > ", INDENT, moves);
+    char c = fgetc(stdin);
+    clear_input();
+    int valid = 0;
+    switch(c) {
+        // In a switch statement we can use multiple cases to do the same
+        //   thing if we place them one after another like this
+        case 'w':
+        case 'W':
+            valid = handle_move(0, -1);
+            break;
+        case 's':
+        case 'S':
+            valid = handle_move(0, 1);
+            break;
+        case 'a':
+        case 'A':
+            valid = handle_move(-1, 0);
+            break;
+        case 'd':
+        case 'D':
+            valid = handle_move(1, 0);
+            break;
+        case 'q':
+        case 'Q':
+            // End the game
+            return 0;
+        default:
+            // if we get here the user entered an invalid char
+            printf("%sInvalid input!\n", INDENT);
+            // return early since we didnt specify a move;
+            return 1;
+    }
+    if(!valid) printf("%sInvalid move!\n", INDENT);
+    return 1;
+}
+
+// We need this to clear out the input buffer if the user enters more characters
+//   than we need. Otherwise further reading from stdin will use the excess
+//   characters instead of asking for input again
+void clear_input() {
+    char c;
+    while(( c = fgetc(stdin)) != '\n' && c != EOF);
+}
+
+// Now the player can actually move pieces around the board and quit the game
+// Next time we'll add a way to check the win condition and test out a larger
+//   square size
